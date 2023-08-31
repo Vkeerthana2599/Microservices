da38f34157f435a5e9a680d3033b4e814422c90e
Vidyapushpa <vidyavishwanath186@gmail.com>
2023-08-31 19:44:29 +0530

    logeer is added to stock services

--- stockservice/src/main/java/com/keerthi/stockservice/service/StockService.java
+++ stockservice/src/main/java/com/keerthi/stockservice/service/StockService.java
@@ -2,8 +2,11 @@ package com.keerthi.stockservice.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.keerthi.stockservice.controller.StockController;
 import com.keerthi.stockservice.entity.Stock;
@@ -15,7 +18,11 @@ public class StockService {
 	@Autowired
 	private StockRepository stockRepository;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(StockService.class);
+	
 	public void saveStockDetails(Stock stock) {
+		
+		LOGGER.info("information");
 		stockRepository.save(stock);
 	}
 	
